8f20aeee11936fe3f202679308934efac7040ccb
Bigoz005 <32684265+Bigoz005@users.noreply.github.com>
2017-10-31 14:48:52 +0100

    final
    final

--- stack/stack.cpp
+++ stack/stack.cpp
@@ -1,24 +1,18 @@
 #include <assert.h>
 #include <stdlib.h>
-#include <stdio.h>
-#include "stack.h"
+#include "stack.h" 
 
 stack::stack()
 {
 	this->top=0;
-	size =1;
-	dane =NULL;
-	dane =(int*)malloc(sizeof(int));
-	if(dane==NULL)
-	{
-	  assert(dane=NULL);
-	}
+	size = 0;
+	dane = (int*)malloc(sizeof(int));
 }
 
 stack::~stack()
 {
-  free(dane);
-  top=0;
+         free(dane);
+         top=0;
 }
 
 void stack::clear()
@@ -26,13 +20,25 @@ void stack::clear()
 	this->top=0;
 }
 
-void stack::push(int a)
+void stack::push(int n)
 {
-  
+
+
+	if (top>=size) {
+		int* newDane = (int*)realloc(dane,(size+1)*sizeof(int));
+		if (newDane) {
+			dane=newDane;
+			size++;
+		} else {
+			free(dane);
+			exit(1);
+		}
+	}
+	dane[top++] = n;
 }
 
 int stack::pop()
 {
-	assert(this->top>0);
-	return this->dane[--this->top];
+	assert(top>0);
+	return dane[--top];
 }
--- stack/stack.h
+++ stack/stack.h
@@ -1,14 +1,15 @@
-include <stdlib.h>
-#define STACKSIZE 20
 class stack
 {
 public:
-	int * dane = (int*) malloc (sizeof(int)*STACKSIZE);
 	void push(int a);
 	int pop();
 	void clear();
+	bool isEmpty();
 	stack();
 	~stack();
+	int *dane;
+	int size;
 private:
 	int top;
+	
 };
