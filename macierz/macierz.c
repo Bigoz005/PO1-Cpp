2b2121d8f98db6bfaabd8ddb057b5f35ab770579
Bigoz005 <32684265+Bigoz005@users.noreply.github.com>
2017-10-31 14:50:33 +0100

    final

--- macierz/macierz.c
+++ macierz/macierz.c
@@ -2,121 +2,90 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
-/* double alokacja (double ***matrix, int wymiar){
-
-  int k=0;
-  matrix=(double**)malloc(sizeof(double*)*(wymiar));
-
-  if(matrix==NULL){
-    free(matrix);
-    printf("blad alokacji");
-    return 0;
-  }
-
-  for (k=0; k<wymiar; k++){
-    matrix[k]=(double*)malloc(sizeof(double)*(wymiar));
-      if(matrix[k]==NULL){
-        free(macierz[k]);
-        printf("blad alokacji");
-        return 0;
-      }
-  }
-} */
-
-double** alokacja (int wymiar){
-
-  int k=0;
-  double **macierz=NULL;
-  macierz=(double**)malloc(sizeof(double*)*(wymiar));
-
-  if(macierz==NULL){
-    free(macierz);
-    printf("blad alokacji");
-    return 0;
-  }
-
-  for (k=0; k<wymiar; k++){
-    macierz[k]=(double*)malloc(sizeof(double)*(wymiar));
-      if(macierz[k]==NULL){
-        free(macierz[k]);
-        printf("blad alokacji");
-        return 0;
-      }
-  }
-  return macierz;
+double **alokacja(int n){
+	int i,j;
+	double **macierz = (double **)malloc(n * sizeof(double*));
+	if(macierz==NULL){
+		printf("Blad alokacji");
+		return 0;
+	}
+	for(i = 0; i < n; i++){
+		macierz[i] = (double *)malloc(n * sizeof(double));
+		if(macierz[i]==NULL){
+		  printf("Blad alokacji");
+		  for(j=0; j < i ;j++){
+			free(macierz[j]);
+		  }
+		  free(macierz);
+		  return 0;
+		}
+	}
+	return macierz;
+}
+void matrix_free(double** minor, int n){
+	int i;
+	for (i=0; i<n; i++){
+		free(minor[i]);
+	}
+	free(minor);
 }
 
-double wyznacznik (double **tab, int size){
+double det(double ** matrix, int n)
+{
+	double ** minor;
+	double sum=0.0;
+	double sign=1.0;
+	int i,j,k;
 
-  int l=0,i=0,j=0, znak=1;
-  double **min=NULL,sum=0;
+	if(n==1){
+		return matrix[0][0];
+	}
+	if(n==2){
+		return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
+	}
 
-  if(size==1){
-    return tab[0][0];
-  }
+	minor = alokacja(n-1);
 
-  if (size==2){
-    sum = tab[0][0]*tab[1][1]-tab[0][1]*tab[1][0];
-  return sum;
-  }
-
-  min=alokacja(size-1);
-
-  for(l=0;l<(size);l++){
-    for(i=0; i<(size-1); i++){
-      for(j=0; j<(size-1);j++){
-        if(j<1)
-          min[i][j]=tab[i+1][j];
-        else
-          min[i][j]=tab[i+1][j+1];
-        }
-      sum+=znak*tab[0][1]*wyznacznik(min, (size-1));
-      znak=-znak;
-    }
-    sum+=znak*tab[0][1]*wyznacznik(min, (size-1));
-    znak=-znak;
-  }
-
-  for(l=0; l<(size-1); l++){
-     free(min[l]);
-  }
-
-  free (min);
-  return sum;
+	for(i=0;i<n;i++){
+		for(j=0; j<n-1; j++){
+			for(k=0; k<n-1;k++){
+				if(i>k){
+					minor[j][k]=matrix[j+1][k];
+				}else{
+					minor[j][k]=matrix[j+1][k+1];
+				}
+			}
+		}
+		sum+=sign*matrix[0][i]*det(minor,n-1);
+		sign=-sign;
+	}
+	matrix_free(minor, n-1);
+	return sum;
 }
 
-int main(void){
-
-  FILE *f;
-  double **macierz=NULL, wynik=0;
-  int wymiar=0, i=0, j=0;
-
-  if( (f=fopen("matrix.txt", "rt")) != NULL){
-    fscanf(f, "%d", &wymiar);
-    macierz = alokacja(wymiar);
-
-    for(i=0; i<wymiar; i++){
-      for(j=0; j<wymiar; j++){
-        fscanf(f, "%lf", &macierz[i][j]);
-        printf("%f\n", macierz[i][j]);
-      }
-    }
-  }
-  else{
-    printf("nie mozna otworzyc pliku");
-    fclose(f);
-    return 0;
-  }
-
-  fclose(f);
-  wynik=wyznacznik(macierz, wymiar);
-  printf("wyznacznik: %d\n", wynik);
-
-  for(i=0; i<wymiar; i++){
-    free(macierz[i]);
-  }
-
-  free(macierz);
-return 0;
+int main(){
+	double **minor;
+	FILE *p;
+	int n = 0, i,j;
+	p = fopen ("matrix.txt","rt");
+	if (p!=NULL)
+	{
+		fscanf(p, "%d", &n);
+		minor = alokacja(n);
+		for(i=0;i<n;i++){
+			for(j=0;j<n;j++){
+				fscanf(p, "%lf", &minor[i][j]);
+			}
+		}
+	fclose (p);
+	printf("%lf", det(minor, n));
+	matrix_free(minor, n);
+	}
+	else{
+		fclose (p);
+		printf("Nie mozna otworzyc pliku");
+	}
+	return 0;
 }
